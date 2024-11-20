539aad35121a36799baec8de9febba9097f331e6
susank-aryl <aryalsusank659@gmail.com>
2024-11-20 04:58:32 +0000

    Updating Getter Setter Test

--- test/hospital.py
+++ test/hospital.py
@@ -3,19 +3,60 @@ class hospital:
     ministry = "Ministry of Health"
 
     # class Constructer
-    def __init__(self,name,location,ministry) -> None: #This function doesnot return anything
+    def __init__(self,name,location,ministry, revenew, patients) -> None: #This function doesnot return anything
     # Initializing Parameters   
         self.name = name
         self.location = location
         self.ministry = ministry
+        self.__revenew = revenew #private attribute
+        self.__patients = patients #private attribute
 
+    def __str__(self) -> str:
+        """
+        Used to provide infomation about the class. It is called when print(object) is passed.
+        """
+        return f"Name : {self.name} | Location : {self.location} | Ministry : {self.ministry}"
+    
+    def get_revenew(self):
+        """
+        Getter method to access the private attribute.
+        """
+        if self.__revenew < 100000:
+            raise ValueError("Too small revenew")
+        return self.__revenew
+    
+    def get_patients(self):
+        """
+        Getter method to access the private attribute patient
+        """
+        return self.__patients
+    
+    def set_revenew(self, revenew):
+        """
+        Setter method to update the private attribute.
+        Always use setter to update the private attribute
+        """
+        if self.__revenew < 100000:
+            raise ValueError("Too small revenew")
+        self.__revenew = revenew
+    
+    def set_patients(self, patients):
+        """
+        Setter method to update the private attribute.
+        """
+        self.__patients = patients
+
+    def avg_money_spent(self):
+        # Avg money spent can only be accessed throught a method
+        return f"${self.__revenew / self.__patients:,.2f}K"
+    
     def facilities(self, beds):
         """
         This will create or update an instance attribute, 
         store the value of bed inside of object making it part of object's status.
         """
         self.beds = beds
-        print("\nThe object instance attribute has been updated + object.beds.")   
+        print("The object instance attribute has been updated + object.beds.")   
 
     def complaints(self, message):
         """
@@ -24,7 +65,7 @@ class hospital:
         """
         print(f"The most common comment for {self.name} is {message}")
 
-grandy = hospital("Grandy" , "Greenland", "DepHealth") #Creating object instance
+grandy = hospital("Grandy" , "Greenland", "DepHealth", 120000, 40000) #Creating object instance
 
 print(f"The name of this hospital is {grandy.name} and is located at {grandy.location}.")
 
@@ -43,3 +84,17 @@ print("=" * 75)
 #Initialing message parameter fir complaint method
 grandy.complaints("too expensive")
 # grandy.message() # Error AttributeError : Has no attribute message
+
+print(f"The revenew of the hospital is {grandy.get_revenew()} with total patients being {grandy.get_patients()} and avg being {grandy.avg_money_spent()}.") #the private element can only be accessed using getter
+
+print("=" * 75)
+
+# Using Getter and Setter
+grandy.set_patients(44700)
+grandy.set_revenew(125000)
+print(f"Past year the total patients were {grandy.get_patients()} and the total revenew was {grandy.get_revenew()}")
+print(f"Same year the average money spent was {grandy.avg_money_spent()}")
+
+print("=" * 75)
+# Getting the class description "runs __str__ method"
+print(grandy)
